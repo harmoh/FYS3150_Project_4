fbffa72be73409406efccc432b17527a7c19d4ff
harmoh <harmoh@me.com>
2016-11-04 13:14:15 +0100

    Monte Carlo cycles and switching between states

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 using namespace arma;
 
 // Declare functions
-void initializeLattice(int nSpins, mat spinMatrix, double &energy, double &magneticMoment);
+void initializeLattice(int nSpins, mat &spinMatrix, double &energy, double &magneticMoment);
 
 // Periodic boundary conditions
 int pbc(int i, int limit, int add)
@@ -23,22 +23,60 @@ int main(int argc, char *argv[])
 
     int nSpins = 2;
     mat spinMatrix = zeros<mat>(nSpins,nSpins);
-    double energy = 0;
-    double magneticMoment = 0;
-
-    cout << "Energy: " << energy << endl;
-    cout << "Magnetic moment: " << magneticMoment << endl;
+    double energy = 0.0;
+    double magneticMoment = 0.0;
+    double temperature = 1.0;
 
     initializeLattice(nSpins, spinMatrix, energy, magneticMoment);
 
-    //cout << "Matrix:\n" << spinMatrix << endl;
     cout << "Energy: " << energy << endl;
     cout << "Magnetic moment: " << magneticMoment << endl;
+
+    vec energyDifference = zeros<mat>(17);
+    for(int dE = -8; dE <= 8; dE += 4)
+    {
+        energyDifference(dE + 8) = exp(-dE/temperature);
+    }
+
+    //cout << "Energy difference:\n" << energyDifference << endl;
+
+    // Monte Carlo cycles
+    int mcCycles = 0;
+    bool final = false;
+    //for(int i = 0; i < mcCycles; i++)
+    while(!final)
+    {
+        // Sweep over the lattice
+        for(int y = 0; y < nSpins; y++)
+        {
+            for(int x = 0; x < nSpins; x++)
+            {
+                int ix = (int) (rand(gen) * (double) nSpins);
+                int iy = (int) (rand(gen) * (double) nSpins);
+                int deltaE = 2 * spinMatrix(ix,iy) *
+                        (spinMatrix(pbc(ix,nSpins,-1),iy) + spinMatrix(ix,pbc(iy,nSpins,-1)) +
+                         spinMatrix(pbc(ix,nSpins,1),iy) + spinMatrix(ix,pbc(iy,nSpins,1)));
+                if(rand(gen) <= energyDifference(deltaE + 8))
+                {
+                    spinMatrix(ix,iy) *= -1.0;
+                    magneticMoment += (double) 2 * spinMatrix(ix,iy);
+                    energy += (double) deltaE;
+                    cout << "DeltaE: " << deltaE << "\t Energy: " << energy << endl;
+                }
+            }
+        }
+        if(magneticMoment == -4) final = true;
+        mcCycles++;
+    }
+
+    cout << "Energy: " << energy << endl;
+    cout << "Magnetic moment: " << magneticMoment << endl;
+    cout << "MC cycles: " << mcCycles << endl;
 
     return 0;
 }
 
-void initializeLattice(int nSpins, mat spinMatrix, double &energy, double &magneticMoment)
+void initializeLattice(int nSpins, mat &spinMatrix, double &energy, double &magneticMoment)
 {
     // Set ground state of the lattice and magnetic moment
     for(int y = 0; y < nSpins; y++)
