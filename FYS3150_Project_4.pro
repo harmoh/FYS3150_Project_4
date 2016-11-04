6c5898f86119922665237b72db79ffdec29ce831
harmoh <harmoh@me.com>
2016-11-04 12:17:48 +0100

    Added basic functionality for finding energy and magnetic momentum of 2x2 lattice

--- FYS3150_Project_4.pro
+++ FYS3150_Project_4.pro
@@ -3,4 +3,9 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
+INCLUDEPATH += /usr/local/Cellar/armadillo/7.400.2/include
+LIBS += -L/usr/local/lib
+
 SOURCES += main.cpp
+
+LIBS += -larmadillo -llapack -lblas
--- main.cpp
+++ main.cpp
@@ -1,9 +1,61 @@
+#include <random>
 #include <iostream>
+#include <armadillo>
 
 using namespace std;
+using namespace arma;
 
+// Declare functions
+void initializeLattice(int nSpins, mat spinMatrix, double &energy, double &magneticMoment);
+
+// Periodic boundary conditions
+int pbc(int i, int limit, int add)
+{
+    return (i + limit + add) % limit;
+}
+
+// Main program
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_real_distribution<double> rand(0.0,1.0);
+
+    int nSpins = 2;
+    mat spinMatrix = zeros<mat>(nSpins,nSpins);
+    double energy = 0;
+    double magneticMoment = 0;
+
+    cout << "Energy: " << energy << endl;
+    cout << "Magnetic moment: " << magneticMoment << endl;
+
+    initializeLattice(nSpins, spinMatrix, energy, magneticMoment);
+
+    //cout << "Matrix:\n" << spinMatrix << endl;
+    cout << "Energy: " << energy << endl;
+    cout << "Magnetic moment: " << magneticMoment << endl;
+
     return 0;
+}
+
+void initializeLattice(int nSpins, mat spinMatrix, double &energy, double &magneticMoment)
+{
+    // Set ground state of the lattice and magnetic moment
+    for(int y = 0; y < nSpins; y++)
+    {
+        for(int x = 0; x < nSpins; x++)
+        {
+            spinMatrix(x,y) = 1.0;
+            magneticMoment += (double) spinMatrix(x,y);
+        }
+    }
+
+    // Set up energy
+    for(int y = 0; y < nSpins; y++)
+    {
+        for(int x = 0; x < nSpins; x++)
+        {
+            energy -= spinMatrix(x,y)*(spinMatrix(pbc(x,nSpins,-1),y) + spinMatrix(x,pbc(y,nSpins,-1)));
+        }
+    }
 }
