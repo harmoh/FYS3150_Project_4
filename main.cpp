f0d109c8dab22ee3fdc1b8ff87668009d5ce70bb
harmoh <harmoh@me.com>
2016-11-08 12:44:31 +0100

    Prints to file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <random>
+#include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <armadillo>
 #include <time.h>
 
 using namespace std;
 using namespace arma;
 
+ofstream ofile;
+
 // Declare functions
 void initializeLattice(int nSpins, mat &spinMatrix, double &energy, double &magneticMoment);
 void metropolis(int nSpins, int mcCycles, double temp, vec &expectationValues);
+void writeToFile(int nSpins, int mcCycles, double temp, vec expectationValues);
 
 // Periodic boundary conditions
 int pbc(int i, int limit, int add)
@@ -31,8 +36,20 @@ int main(int argc, char *argv[])
 
     // Monte Carlo cycles
     int mcCycles = atoi(argv[1]);
+
+    // Declare new file name and add lattice size to file name
+    string fileout = "Lattice";
+    fileout.append(to_string(nSpins) + ".txt");
+    ofile.open(fileout);
+
     clock_t time_initial = clock();
+
+    // Start Metropolis algorithm with Monte Carlo sampling
     metropolis(nSpins, mcCycles, temp, expectationValues);
+    writeToFile(nSpins, mcCycles, temp, expectationValues);
+
+    ofile.close();
+
     clock_t time_final = clock();
     double elapsed_time = (time_final - time_initial) / (double) CLOCKS_PER_SEC;
 
@@ -41,6 +58,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+// Initalize lattice with spins, magnetic moment and energy
 void initializeLattice(int nSpins, mat &spinMatrix, double &energy, double &magneticMoment)
 {
     // Set ground state of the lattice and magnetic moment
@@ -63,6 +81,7 @@ void initializeLattice(int nSpins, mat &spinMatrix, double &energy, double &magn
     }
 }
 
+// Perform the Metropolis algorithm
 void metropolis(int nSpins, int mcCycles, double temp, vec &expectationValues)
 {
     // Initialize RNG, can be called by rand(gen) to get a random number between 0 and 1
@@ -120,9 +139,16 @@ void metropolis(int nSpins, int mcCycles, double temp, vec &expectationValues)
     cout << "Magnetic moment: " << magneticMoment << endl;
     cout << "MC cycles: " << mcCycles << endl;
 
-    // Numerical values
+
+}
+
+void writeToFile(int nSpins, int mcCycles, double temp, vec expectationValues)
+{
+    // Normalization of the values
     double norm = 1.0/mcCycles;
     double normSpins = 1.0 / nSpins / nSpins;
+
+    // Numerical values
     double expectVal_E = expectationValues(0)*norm;// / nSpins / nSpins;
     double expectVal_E2 = expectationValues(1)*norm;
     double expectVal_Eabs = expectationValues(2)*norm;
@@ -130,7 +156,7 @@ void metropolis(int nSpins, int mcCycles, double temp, vec &expectationValues)
     double expectVal_M2 = expectationValues(4)*norm;// / nSpins / nSpins;
     double expectVal_Mabs = expectationValues(5)*norm;
 
-    double expectVal_Cv = (expectVal_E2 - expectVal_Eabs * expectVal_Eabs) * normSpins / temp;
+    double expectVal_Cv = (expectVal_E2 - expectVal_Eabs * expectVal_Eabs) * normSpins / (temp * temp);
     double expectVal_X = (expectVal_M2 - expectVal_Mabs * expectVal_Mabs) * normSpins / temp;
 
     cout << "\nExpectation values, numerical: " << endl;
@@ -147,15 +173,14 @@ void metropolis(int nSpins, int mcCycles, double temp, vec &expectationValues)
     // Analytical values
     double J = 1.0;
     double beta = 1.0;
-    double kbTemp = 1.0;
     double Z = 4*cosh(8*J*beta) + 12;
     double expectValAnalytical_E = 32*J*sinh(8*J*beta) / Z;
     double expectValAnalytical_M2 = (32*exp(8*J*beta) + 32) / Z;
     double expectValAnalytical_Mabs = (8*exp(8*J*beta) + 16) / Z;
     double expectValAnalytical_Cv = ((256*J*cosh(8*J*beta)) / Z - expectValAnalytical_E *
-                                     expectValAnalytical_E) * normSpins / kbTemp;
+                                     expectValAnalytical_E) * normSpins / (temp * temp);
     double expectValAnalytical_X = (expectValAnalytical_M2 - expectValAnalytical_Mabs *
-                                    expectValAnalytical_Mabs) * normSpins / kbTemp;
+                                    expectValAnalytical_Mabs) * normSpins / temp;
     //double Achi = 8*(exp(8.0/temp) + cosh(8.0/temp) + 3.0/2.0)/(temp*pow((cosh(8.0/temp)+3), 2));
 
     cout << "\nExpectation values, analytical:" << endl;
@@ -167,4 +192,19 @@ void metropolis(int nSpins, int mcCycles, double temp, vec &expectationValues)
     // Error between numerical and analytical
     cout << "Heat capacity (error): " << fabs(expectValAnalytical_Cv - expectVal_Cv) << endl;
     cout << "Susceptibility (error): " << fabs(expectValAnalytical_X - expectVal_X) << endl;
+
+    ofile << setiosflags(ios::showpoint | ios::uppercase);
+    ofile << setw(15) << setprecision(8) << temp;
+    ofile << setw(15) << setprecision(8) << expectVal_E * normSpins;
+    ofile << setw(15) << setprecision(8) << expectVal_Cv;
+    ofile << setw(15) << setprecision(8) << expectVal_M;
+    ofile << setw(15) << setprecision(8) << expectVal_X;
+
+    /*
+    ofile << setw(15) << setprecision(8) << E_ExpectationValues/NSpins/NSpins;
+    ofile << setw(15) << setprecision(8) << Evariance/temperature/temperature;
+    ofile << setw(15) << setprecision(8) << M_ExpectationValues/NSpins/NSpins;
+    ofile << setw(15) << setprecision(8) << Mvariance/temperature;
+    ofile << setw(15) << setprecision(8) << Mabs_ExpectationValues/NSpins/NSpins << endl;
+    */
 }
